a582a3132c576b094392c41eb812d34ed3abecb7
benjaminapetersen <admin@benjaminapetersen.me>
2016-08-16 16:46:18 -0400

    Add first unit test for directive

--- test/spec/directives/key-value-editor.spec.js
+++ test/spec/directives/key-value-editor.spec.js
@@ -1,5 +1,36 @@
 'use strict';
 
-describe('keyValueEditor Directive', function() {
+describe('keyValueEditor', function() {
+  var scope;
+  var element;
+
+  beforeEach(module('key-value-editor'));
+
+  beforeEach(inject(function($rootScope, $compile) {
+    scope = $rootScope.$new();
+    scope.entries = [];
+
+    element = angular.element('<key-value-editor entries="entries"></key-value-editor>');
+    $compile(element)(scope);
+    $('body').append(element);
+    scope.$apply();
+  }));
+
+  afterEach(function() {
+    $('body').empty();
+  });
+
+  describe('when given a list of one entry', function() {
+    it('should display the name and value of that entry', function() {
+      scope.entries.push({
+        name: 'foo',
+        value: 'bar'
+      });
+      scope.$apply();
+      expect($('input:text').eq(0).val()).toEqual('foo');
+      expect($('input:text').eq(1).val()).toEqual('bar');
+    });
+  });
+
 
 });
